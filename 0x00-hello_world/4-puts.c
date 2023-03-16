a8ed306d4e8f744ddc55e4d226b6a25796ca0048
bennyhosea <benjamin.jaywon@yahoo.com>
2023-03-16 19:02:40 +0000

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return(0);
+	return (0);
 }
