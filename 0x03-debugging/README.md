3509ba5404ace5f024b1f4ed8018b6763bae57eb
bennyhosea <benjamin.jaywon@yahoo.com>
2023-03-23 08:53:17 +0000

    multiple main

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-0x03 - Debugging
+0x03 - debugging
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,10 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int main(void)
 int largest_number(int a, int b, int c)
 int convert_day(int month, int day)
 void print_remaining_days(int month, int day, int year)
