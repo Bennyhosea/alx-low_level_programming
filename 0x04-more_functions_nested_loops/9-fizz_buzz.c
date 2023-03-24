feb53a776d4f23e564cbdddcfffd8466c2b0aac5
bennyhosea <benjamin.jaywon@yajoo.com>
2023-03-24 17:43:37 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print a Fizz Buzz program
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+	if ((n % 3 == 0) && (n % 5 == 0))
+	{
+	printf("FizzBuzz");
+	}
+	else if (n % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (n % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", n);
+	}
+	if (n != 100)
+	{
+	printf(" ");
+	}
+	}
+	printf("\n");
+	return (0);
+}
