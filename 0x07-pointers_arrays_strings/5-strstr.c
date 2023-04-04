31ee9562f71f81dd920df211b2ce4a8e4e2866e9
bennyhosea <benjamin.jaywon@yajoo.com>
2023-04-04 00:49:38 +0000

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - function that locate a subsring
+ * @haystack: input
+ * @needle: input
+ * Return: slways 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
