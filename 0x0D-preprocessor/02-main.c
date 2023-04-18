88d0277fdf4762e431050f9aabc279ad79a65f03
bennyhosea <benjamin.jaywon@yajoo.com>
2023-04-18 09:42:18 +0000

    __FILE__

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,8 +1,12 @@
-#include <stdlib.h>
 #include <stdio.h>
+/**
+ * main - print the name of the file it was compile from
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	printf("%s\n", __FILE__ );
+	printf("%s\n", __FILE__);
 	return (0);
 }
