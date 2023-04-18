7300022371248eceda033a41b9829d53eeb3255d
bennyhosea <benjamin.jaywon@yajoo.com>
2023-04-18 11:04:55 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's information
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Long description
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+/**
+ * dog_t - typede for struct dog
+ */
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
