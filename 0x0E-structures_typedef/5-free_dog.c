b37ef0d9481dc8366c1b3c58d5d2ff762d5d698b
bennyhosea <benjamin.jaywon@yajoo.com>
2023-04-19 00:51:32 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dogs
+ * @d: the dog to be freed
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
